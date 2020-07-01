08bf179c922dd0d93294a109ceb7dd105fbbc8fe
bheki-maenetja <bhekimaenetja@gmail.com>
2020-07-01 13:24:13 +0200

    completed factorial in c

--- c/k7_factorial.c
+++ c/k7_factorial.c
@@ -5,13 +5,29 @@ int factorial(int a);
 int main(void)
 {
   int my_num;
-  printf("Enter a number: \n");
+  printf("Enter a number: ");
   scanf("%d", &my_num);
   int my_factorial = factorial(my_num);
-  printf("The factorial of %d is %d", my_num, my_factorial);
+  printf("The factorial of %d is %d\n", my_num, my_factorial);
 }
 
 int factorial(int n)
 {
-  return n;
+  if (n < 0 || n > 12)
+  {
+    return -1;
+  }
+  else if (n == 0)
+  {
+    return 1;
+  }
+  
+  if (n == 1)
+  {
+    return n;
+  }
+  else
+  {
+    return n * factorial(n - 1);
+  }
 }
